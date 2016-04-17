2bae06847c5ecbd5ffa8eed6fb8225a197e06175
Youngil Cho <terzeron@gmail.com>
2016-04-18 00:08:45 +0900

    add a project for thymeleaf test

---
+++ SpringBootTest3/src/main/java/net/terzeron/SpringBootTest3Application.java
@@ -0,0 +1,12 @@
+package net.terzeron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest3Application.class, args);
+	}
+}
