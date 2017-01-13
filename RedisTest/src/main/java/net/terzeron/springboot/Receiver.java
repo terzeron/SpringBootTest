0b2da69808e3f4ef8fc5e4fa721d808390603732
Young-il Cho <youngilcho@nhnent.com>
2017-01-13 12:15:30 +0900

    add a redis test example

---
+++ RedisTest/src/main/java/net/terzeron/springboot/Receiver.java
@@ -0,0 +1,27 @@
+package net.terzeron.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by terzeron on 2017. 1. 12..
+ */
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+
+}
