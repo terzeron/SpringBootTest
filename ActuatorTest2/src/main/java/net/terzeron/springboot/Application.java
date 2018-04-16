15aeefddd57101f5867a50c06adce2a4c1bb15be
Young-il Cho <youngilcho@nhnent.com>
2018-04-16 19:16:03 +0900

    add a actuator test example

---
+++ ActuatorTest2/src/main/java/net/terzeron/springboot/Application.java
@@ -0,0 +1,30 @@
+package net.terzeron.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // with admin/admin
+    // http://localhost:8081/details/actuator
+    // http://localhost:8081/details/autoconfig    *
+    // http://localhost:8081/details/beans         *
+    // http://localhost:8081/details/configprops   *
+    // http://localhost:8081/details/dump          *
+    // http://localhost:8081/details/env           *
+    // http://localhost:8081/details/health        *
+    // http://localhost:8081/details/info          *
+    // http://localhost:8081/details/metrics       *
+    // http://localhost:8081/details/mappings      *
+    // http://localhost:8081/details/shutdown
+    // http://localhost:8081/details/trace         *
+    // http://localhost:8081/details/logfile
+    // http://localhost:8081/details/flyway
+    // http://localhost:8081/details/liquibase
+
+    // application.properties에 설정이 켜진 endpoint 위주로 접근이 가능함
+}
---
+++ ActuatorTest2/src/main/java/net/terzeron/springboot/CustomHealthCheck.java
@@ -0,0 +1,17 @@
+package net.terzeron.springboot;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthCheck implements HealthIndicator {
+    public Health health() {
+        int errorCode = 0;
+        //int errorCode = 1;
+        if (errorCode != 1) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+}
