8c794352555b32c51253b0f7df489c5c34bf307a
Young-il Cho <youngilcho@nhnent.com>
2018-06-15 11:04:50 +0900

    add a new module

--- LoggingTest/src/main/java/net/terzeron/springboot/model/TestModel.java
+++ LoggingTest/src/main/java/net/terzeron/springboot/model/TestModel.java
@@ -1,4 +1,13 @@
 package net.terzeron.springboot.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestModel {
+    public static final Logger logger = LoggerFactory.getLogger(TestModel.class);
+
+    public TestModel() {
+        logger.debug("log message at DEBUG level from TestModel constructor"); // ignored due to logger level
+        logger.info("log message at INFO level from TestModel constructor");
+    }
 }
--- LoggingTest/src/main/java/net/terzeron/springboot/service/TestService.java
+++ LoggingTest/src/main/java/net/terzeron/springboot/service/TestService.java
@@ -1,4 +1,13 @@
 package net.terzeron.springboot.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestService {
+    private Logger logger = LoggerFactory.getLogger(TestService.class);
+
+    public void service() {
+        logger.info("message at INFO level from TestService.service()"); // ignored due to logger level
+        logger.warn("message at WARN level from TestService.service()");
+    }
 }
