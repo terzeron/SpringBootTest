2d24ceb9e5f43daecae2dcf83f60e7fc9ee4631b
Young-il Cho <youngilcho@nhnent.com>
2018-08-03 17:45:40 +0900

    add a new module for simple data service on JPA

---
+++ EmployeeWebService/src/main/java/net/terzeron/springboot/EmployeeController.java
@@ -0,0 +1,29 @@
+package net.terzeron.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public List<Employee> listEmployees() {
+        List<Employee> employee = employeeService.getList();
+        return employee;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public void addEmployee(@RequestParam(value = "employeeName", required = false) String employeeName,
+                            @RequestParam(value = "employeeId", required = false) String employeeId,
+                            @RequestParam(value = "employeeCity", required = false) String employeeCity) {
+        Employee employee = new Employee(employeeId, employeeName, employeeCity);
+        employeeService.save(employee);
+    }
+}
