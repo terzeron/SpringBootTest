23248c8e01d1fefc0160a889d4873a21f8336559
Young-il Cho <youngilcho@nhnent.com>
2018-09-05 17:52:45 +0900

    modify actuator urls

--- ActuatorTest/src/main/java/net/terzeron/springboot/Main.java
+++ ActuatorTest/src/main/java/net/terzeron/springboot/Main.java
@@ -7,14 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by terzeron on 2017. 1. 13..
  */
 @SpringBootApplication
-public class HelloWorldConfiguration {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldConfiguration.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
     // http://www.baeldung.com/spring-boot-actuators
-    // http://localhost:9001/health
-    // http://localhost:9001/metric
-    // http://localhost:9001/configprops
-
+    // http://localhost:9000/hello-world
+    // http://localhost:9000/actuator/
+    // http://localhost:9000/actuator/health
+    // http://localhost:9000/actuator/info
 }
