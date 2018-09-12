def3cdd4cc4fc18e9542513e7bb7c68eb685cf69
Young-il Cho <youngilcho@nhnent.com>
2018-09-12 09:44:01 +0900

    add two class files

--- StateMachineTest/src/main/java/net/terzeron/springboot/Application.java
+++ StateMachineTest/src/main/java/net/terzeron/springboot/Application.java
@@ -1,4 +1,23 @@
 package net.terzeron.springboot;
 
-public class Applicaiton {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    @Autowired
+    private StateMachine<States, Events> stateMachine;
+
+    @Override
+    public void run(String... args) throws Exception {
+        stateMachine.sendEvent(Events.E1);
+        stateMachine.sendEvent(Events.E2);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
