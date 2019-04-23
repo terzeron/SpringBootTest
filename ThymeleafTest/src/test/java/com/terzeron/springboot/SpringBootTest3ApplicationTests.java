8e2735319d133abf35f17c136c26d487b210f025
Young-il Cho <youngilcho@nhnent.com>
2019-04-23 14:00:10 +0900

    rearrange project structure to multi-module project

--- ThymeleafTest/src/test/java/com/terzeron/springboot/SpringBootTest3ApplicationTests.java
+++ ThymeleafTest/src/test/java/com/terzeron/springboot/SpringBootTest3ApplicationTests.java
@@ -2,17 +2,13 @@ package com.terzeron.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootTest3Application.class)
 @WebAppConfiguration
 public class SpringBootTest3ApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
