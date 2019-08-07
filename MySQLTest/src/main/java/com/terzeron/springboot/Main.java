4cae8092d9285e22e6732172ca44ddc1dec8ca7e
Young-il Cho <youngilcho@nhnent.com>
2019-08-07 14:28:36 +0900

    update mysql-connector-java version add some comments modify jdbc driver class type

--- MySQLTest/src/main/java/com/terzeron/springboot/Main.java
+++ MySQLTest/src/main/java/com/terzeron/springboot/Main.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // https://javabeat.net/spring-data-jpa/
 // https://javabeat.net/spring-data-jpa-query/
 
+/*
+ * mysql이 설치되고 실행되어야 함(brew install mysql; brew services start mysql)
+ * schema 생성하고 데이터 insert해야 함
+ * curl http://localhost/books/
+ */
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
