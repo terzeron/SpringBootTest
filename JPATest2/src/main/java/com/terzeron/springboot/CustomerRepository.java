7edc13e8835c4faefe71f64f09478a96dc353acb
Young-il Cho <youngilcho@nhnent.com>
2019-08-27 14:46:21 +0900

    add an example module

---
+++ JPATest2/src/main/java/com/terzeron/springboot/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.terzeron.springboot;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
