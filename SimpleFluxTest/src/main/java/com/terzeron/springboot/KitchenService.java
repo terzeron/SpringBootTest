09874930b60dc7993412c745a8b1890e7fc624a9
Youngil Cho <terzeron@gmail.com>
2022-02-10 14:16:24 +0900

    remove a module

--- SimpleFluxTest/src/main/java/com/terzeron/springboot/KitchenService.java
+++
@@ -1,9 +0,0 @@
-package com.terzeron.springboot;
-
-import reactor.core.publisher.Flux;
-
-public class KitchenService {
-    Flux<Dish> getDishes() {
-        return Flux.just(new Dish("Sesame chicken"), new Dish("Lo mein noodles, plain"), new Dish("Sweet & sour beef"));
-    }
-}
--- SimpleFluxTest/src/main/java/com/terzeron/springboot/SimpleFluxTest.java
+++
@@ -1,8 +0,0 @@
-package com.terzeron.springboot;
-
-public class SimpleFluxTest {
-    public static void main(String[] args) {
-        PoliteServer server = new PoliteServer(new KitchenService());
-        server.doingMyJob().subscribe();
-    }
-}
