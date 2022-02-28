ac0f5eff19bd7b6bd568e28d113d57355d541e9f
Youngil Cho <terzeron@gmail.com>
2022-02-28 14:56:58 +0900

    add logging to flux stream add blockhound feature

--- ReactiveMongoDataStore/src/main/java/com/terzeron/springboot/CartService.java
+++ ReactiveMongoDataStore/src/main/java/com/terzeron/springboot/CartService.java
@@ -13,7 +13,9 @@ class CartService {
     Mono<Cart> addToCart(String cartId, String id) {
         return this.cartRepository
                 .findById("My Cart")
+                .log("found cart")
                 .defaultIfEmpty(new Cart(cartId)) // 카트를 찾아보고 없으면 새로 생성
+                .log("empty cart")
                 .flatMap(cart -> cart.getCartItems().stream() // 카트 상품 목록에 대해
                         .filter(cartItem -> cartItem
                                 .getItem()
@@ -25,11 +27,17 @@ class CartService {
                             return Mono.just(cart);
                         }) // 각각에 대해서 카트 상품의 수량을 하나 증가시키고 그 카트를 반환함
                         .orElseGet(() -> this.itemRepository.findById(id)
+                                .log("fetched item")
                                 .map(item -> new CartItem(item))
+                                .log("cart item")
                                 .doOnNext(cartItem ->
                                         cart.getCartItems().add(cartItem))
+                                .log("added item")
                                 .map(cartItem -> cart)))
                         // 카트에 일치하는 카트 상품이 없으면, id로 일치하는 상품을 찾아서 각각 카트 상품으로 만들고, 카트에 추가
-                .flatMap(this.cartRepository::save); // 카트를 저장하고 반환ᆸ
+                .log("cart with another item")
+                .flatMap(this.cartRepository::save) // 카트를 저장하고 반환
+                .log("saved cart");
+
     }
 }
--- ReactiveMongoDataStore/src/main/java/com/terzeron/springboot/ReactiveMongoDataStore.java
+++ ReactiveMongoDataStore/src/main/java/com/terzeron/springboot/ReactiveMongoDataStore.java
@@ -2,10 +2,16 @@ package com.terzeron.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class ReactiveMongoDataStore {
     public static void main(String[] args) {
+        //Hooks.onOperatorDebug();
+        BlockHound.builder()
+                .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(),"process")
+                .install();
         SpringApplication.run(ReactiveMongoDataStore.class, args);
     }
 }
