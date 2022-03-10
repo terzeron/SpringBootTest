af67b0610997c71d32fccf04c5e5fb386636bd28
Youngil Cho <terzeron@gmail.com>
2022-03-10 14:29:40 +0900

    remove two modules

--- ReactiveMongoDataStoreWithBlockHound/src/main/java/com/terzeron/springboot/CartRepository.java
+++
@@ -1,6 +0,0 @@
-package com.terzeron.springboot;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface CartRepository extends ReactiveCrudRepository<Cart, String> {
-}
